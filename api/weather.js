6b5504fb857780696b46ade031a82083b4806128
louise <dachtedreams@gmail.com@gmail.com>
2019-10-11 13:49:14 +0800

    please

--- api/weather.js
+++ api/weather.js
@@ -1,13 +1,14 @@
 $(function () {
     $.ajax({
-          url: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-ABDE2F5E-A02F-419D-B29B-EC212EFAEDC6&format=JSON&startTime=',
+          url: 'https://www.tianqiapi.com/api/?version=v1&appid=test&appsecret=test',
                     type: 'get',
                     dataType: 'json',
                     error: function(resp, status) {
                         console.log('请求失败：', status, resp);
                     },
                     success: function(resp, status) {
-                        var wCity = resp.result;
+                        console.log('yes');
+                        var wCity = resp.data[1].date;
                         alert(wCity);
                         console.log(wCity);
                         
