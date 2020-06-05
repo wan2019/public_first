dfb3390a0f949571863e35d7bcc373dbb2b563b1
louise <dachtedreams@gmail.com@gmail.com>
2020-06-05 23:16:08 +0800

    [style] 整理scss

--- louise_web_3/js/header.js
+++ louise_web_3/js/header.js
@@ -1,19 +1,23 @@
-function w3_open() {  document.getElementById("mySidebar").style.display = "block";  }
-function w3_close() {  document.getElementById("mySidebar").style.display = "none";  }
-$(document).ready(function(){
 function w3_open() {
-  document.getElementById("mySidebar").style.width = "40%";
-  document.getElementById("mySidebar").style.display = "block";
+    document.getElementById('mySidebar').style.display = 'block';
 }
-
 function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+    document.getElementById('mySidebar').style.display = 'none';
 }
-function setHeaderHight(){
-    if($('#menu').is(':visible')){
-        var targetHigh=$('header .logo').innerHeight();
-        $('header .container').css('height',targetHigh);
-        
-    }console.log('1');
+$(document).ready(function () {
+    function w3_open() {
+        document.getElementById('mySidebar').style.width = '40%';
+        document.getElementById('mySidebar').style.display = 'block';
+    }
+
+    function w3_close() {
+        document.getElementById('mySidebar').style.display = 'none';
+    }
+    function setHeaderHight() {
+        if ($('#menu').is(':visible')) {
+            var targetHigh = $('header .logo').innerHeight();
+            $('header .container').css('height', targetHigh);
+        }
+        console.log('1');
     }
 });
